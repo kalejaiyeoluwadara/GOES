83f9d307cc5c5ec89bf224343290442fc8fa3b60
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-12 09:20:05 +0100

    updates

---
+++ app/components/Nav.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="w-screen h-[60px] bg-white ">
+      <div>
+        <h4>Logo</h4>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ app/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="main">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default Hero;
