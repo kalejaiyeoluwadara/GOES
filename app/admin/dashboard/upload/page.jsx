69a2f36dec3557c84b3a2ed3b5a839698afdf697
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-17 14:17:37 +0100

    updates

--- app/admin/dashboard/upload/page.jsx
+++ app/admin/dashboard/upload/page.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BsCopy } from "react-icons/bs";
 
 function Page() {
-  return <div className="main">Upload page</div>;
+  return (
+    <div className=" h-screen flex items-center justify-center w-screen flex-col py-10 px-8 gap-12">
+      <div className="bg-blue-50 w-[600px] h-[700px] flex items-center justify-center ">
+        <p className="text-primary">Drag and drop file or click to pick file</p>
+      </div>
+      <textarea
+        className="h-[300px] w-[300px] border border-primary "
+        name=""
+        id=""
+      ></textarea>
+      <div></div>
+      <button className=" w-[400px] h-[60px] flex items-center justify-center text-[20px] font-semibold ">
+        Upload
+      </button>
+    </div>
+  );
 }
 
 export default Page;
