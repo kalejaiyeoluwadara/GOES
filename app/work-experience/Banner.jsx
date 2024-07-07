07eaf2de65b4a9724a7a95f7b828b2d3fa92b3da
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-07-07 09:40:34 +0100

    updates

--- app/work-experience/Banner.jsx
+++ app/work-experience/Banner.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import finished from "../assets/banner.jpeg";
 function Banner() {
   return (
-    <main className="h-[300px] relative main  ">
+    <main className="h-[300px]  relative main  ">
       <Image
         placeholder="blur"
-        className="cover object-top  "
+        className="cover object-center  "
         alt=""
         src={finished}
       />
       <div className="cover bg-black bg-opacity-15  " />
-      <h1 className="sm:text-[50px] sm:mt-0 mt-12 text-[30px] text-center font-bold font-600 relative z-40 text-white ">
+      <h1 className="sm:text-[50px] sm:mt-12 mt-12 text-[30px]  text-center font-bold font-600 relative z-40 text-white ">
         Work Experience & Training
       </h1>
     </main>
