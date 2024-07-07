19dae6f91d3c1b6efd90508a628d5b1b0b8b1f4f
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-07-07 09:19:00 +0100

    styling

--- app/equipment-gallery/page.jsx
+++ app/equipment-gallery/page.jsx
@@ -13,15 +13,17 @@ const Grid = ({ styles }) => {
 function Page() {
   return (
     <>
-      <div className="min-h-screen py-20 w-screen bg-white flex flex-col justify-center items-center ">
-        <h1 className="font-medium text-primary ">Equipment Gallery</h1>
-        <div className=" sm:mt-10 grid grid-cols-3 grid-rows-3 gap-4 ">
+      <div className="min-h-screen py-20 sm:py-40 w-screen bg-white flex flex-col justify-center items-center ">
+        <h1 className="font-medium text-primary sm:my-0 my-8 ">
+          Equipment Gallery
+        </h1>
+        <div className=" sm:mt-10 grid sm:grid-cols-3 grid-cols-1 grid-rows-1 sm:grid-rows-3 gap-4 ">
           <Grid styles={"h-[250px]"} />
           <Grid styles={" h-[310px] "} />
           <Grid styles={"h-[250px]"} />
           <Grid styles={"h-[250px]"} />
-          <Grid styles={" h-[220px] mt-8 w-auto col-span-2  "} />
-          <Grid styles={"h-[250px] w-auto col-span-3"} />
+          <Grid styles={" h-[220px] mt-8 w-auto sm:col-span-2  "} />
+          <Grid styles={"h-[250px] w-auto sm:col-span-3"} />
         </div>
       </div>
       <Footer />
--- app/hero/Hero.jsx
+++ app/hero/Hero.jsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import hero from "../assets/hero.jpeg";
+import test1 from "@/app/assets/test1.jpg";
+import test2 from "@/app/assets/test2.jpg";
+import test3 from "@/app/assets/test3.jpg";
 import Button from "../components/Button";
+import Link from "next/link";
 function Hero() {
   return (
     <div className="main sm:px-0 px-4 sm:pt-0 pt-12 relative min-h-[800px] text-white ">
@@ -16,15 +20,24 @@ function Hero() {
           Where <span className="bold text-primary ">Imagination</span> meets{" "}
           <span className="bold text-primary ">Innovation</span>
         </h1>
-        <button className="bg-primary text-white px-6 py-3 mt-4 text-[20px] font-[500] rounded-md  ">
-          Connect With Us
-        </button>
+        <Link href={"/contact"}>
+          {" "}
+          <button className="bg-primary text-white px-6 py-3 mt-4 text-[20px] font-[500] rounded-md  ">
+            Connect With Us
+          </button>
+        </Link>
         {/* Client slide */}
         <div className="flex mt-6 bg-white shadow-sm pl-4 py-2  rounded-xl items-center gap-1  ">
           <section className="flex">
-            <div className="h-[40px] w-[40px] rounded-[50%] bg-white border-[3px] border-primary "></div>
-            <div className="h-[40px] w-[40px] -translate-x-4 rounded-[50%] bg-white border-[3px] border-primary "></div>
-            <div className="h-[40px] w-[40px] -translate-x-8 rounded-[50%] bg-white border-[3px] border-primary "></div>
+            <div className="h-[40px] parent w-[40px] rounded-[50%] bg-white border-[3px] border-primary ">
+              <Image src={test1} alt="" className=" cover " />
+            </div>
+            <div className="h-[40px] parent w-[40px] -translate-x-4 rounded-[50%] bg-white border-[3px] border-primary ">
+              <Image src={test2} alt="" className=" cover " />
+            </div>
+            <div className="h-[40px] w-[40px] parent -translate-x-8 rounded-[50%] bg-white border-[3px] border-primary ">
+              <Image src={test3} alt="" className=" cover " />
+            </div>
           </section>
           <p className="text-primary font-[500] text-[15px] -translate-x-8 ">
             20+ Satisfied Clients
